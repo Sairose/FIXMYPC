132275d2b36326ed7d57df52286ee9d851e7b30f
Sairose Shrestha <sairosestha40@gmail.com>
2025-09-05 22:09:53 +0545

    fix url of axios.js in frontend

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000',
-    // baseURL: 'https://fixmypc-u4qb.onrender.com',
+    // baseURL: 'http://localhost:4000',
+    baseURL: 'https://fixmypc-u4qb.onrender.com',
     withCredentials: true, // to allow cookies to be sent with requests
 })
 
